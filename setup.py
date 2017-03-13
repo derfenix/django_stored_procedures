3a92e127a09d30899179f1724d7b19af6d553601
derfenix <derfenix@gmail.com>
2017-03-13 12:58:38 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django_stored_procedures',
-    version='0.1b',
+    version='0.1',
     keywords=['django', 'stored procedures', 'database'],
     packages=['django_sp'],
     url='https://github.com/derfenix/django_stored_procedures/',
@@ -13,7 +13,7 @@ setup(
     install_requires=['django>=1.7'],
     classifiers=[
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
