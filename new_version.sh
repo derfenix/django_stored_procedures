5feb111099e4400579c404ef135b3a18e49ae8cf
derfenix <derfenix@gmail.com>
2017-06-12 19:51:16 +0300

    Bump version

--- new_version.sh
+++ new_version.sh
@@ -13,4 +13,4 @@ gpg2 --detach-sign -a dist/django_stored_procedures-"${VERSION}".tar.gz
 twine upload dist/django_stored_procedures-"$VERSION".tar.gz dist/django_stored_procedures-"$VERSION".tar.gz.asc
 git commit -a -m "Bump version"
 git tag v${VERSION}
-git push
+git push --tags
